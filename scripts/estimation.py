43eab364ea75b84ec98e88c61b5783b25bc304ee
Joseph Mirabel <jmirabel@laas.fr>
2019-06-27 15:59:14 +0200

    Enrich estimation script.

--- scripts/estimation.py
+++ scripts/estimation.py
@@ -8,15 +8,37 @@ if "--debug" in sys.argv:
 
 rospy.init_node('estimation', log_level = log_level)
 
-try: 
+topic="/joint_states"
+try:
   idx = sys.argv.index("--joint-state")
-  topic = sys.argv[idx+1]
-except ValueError:
-  topic="/joint_states"
+  try:
+    topic = sys.argv[idx+1]
+  except ValueError as e:
+    rospy.logerr ("Could not set joint state topic: " + str(e))
+except ValueError as e:
+    pass
 
+estimation_rate = 50
+try:
+  idx = sys.argv.index("--estimation-rate")
+  try:
+    estimation_rate = int (sys.argv[idx+1])
+  except ValueError as e:
+    rospy.logerr ("Could not set estimation rate: " + str(e))
+except ValueError as e:
+    pass
+rospy.loginfo ("Estimation rate: " + str(estimation_rate))
+
+visual_tags_enabled = True
+if "--disable-visual-tag" in sys.argv:
+    visual_tags_enabled = False
+
+continuous_estimation = False
 if "--continuous-estimation" in sys.argv:
-  _est = est.Estimation (True, joint_states_topic=topic)
-else:
-  _est = est.Estimation (False, joint_states_topic=topic)
+    continuous_estimation = True
 
+_est = est.Estimation (continuous_estimation=continuous_estimation,
+        joint_states_topic=topic,
+        visual_tags_enabled=visual_tags_enabled)
+_est.estimation_rate = estimation_rate
 _est.spin()
