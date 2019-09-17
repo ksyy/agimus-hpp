5926c4d4a7da74810061c4856779da6374229e0b
Alexis Nicolin <alexisnicolin@gmail.com>
2019-09-17 14:32:14 +0200

    Use argparse instead of sys.argv for clarity

--- scripts/estimation.py
+++ scripts/estimation.py
@@ -1,44 +1,31 @@
 #!/usr/bin/env python
-import rospy, sys
+import sys
+import argparse
+
+import rospy
+
 import agimus_hpp.estimation as est
 
-log_level = rospy.INFO
-if "--debug" in sys.argv:
-    log_level = rospy.DEBUG
+parser = argparse.ArgumentParser()
 
-rospy.init_node('estimation', log_level = log_level)
+parser.add_argument("-d", "--debug", action="store_true", help="Set logging level to Debug")
+parser.add_argument("--disable_visual_tags", action="store_true", help="Disable the usage of visual tags")
+parser.add_argument("--continuous_estimation", action="store_true", help="Enable continuous estimation")
+parser.add_argument(
+    "--joint_state", type=str, default="/joint_states", help="Name of the ROS Topic publishing the joint states"
+)
+parser.add_argument("--estimation_rate", type=int, default=50, help="Frequency of the estimation publishing")
 
-topic="/joint_states"
-try:
-  idx = sys.argv.index("--joint-state")
-  try:
-    topic = sys.argv[idx+1]
-  except ValueError as e:
-    rospy.logerr ("Could not set joint state topic: " + str(e))
-except ValueError as e:
-    pass
+args = parser.parse_args()
 
-estimation_rate = 50
-try:
-  idx = sys.argv.index("--estimation-rate")
-  try:
-    estimation_rate = int (sys.argv[idx+1])
-  except ValueError as e:
-    rospy.logerr ("Could not set estimation rate: " + str(e))
-except ValueError as e:
-    pass
-rospy.loginfo ("Estimation rate: " + str(estimation_rate))
+log_level = rospy.DEBUG if args.debug else rospy.INFO
+rospy.init_node("estimation", log_level=log_level)
+rospy.loginfo("Estimation rate: " + str(args.estimation_rate))
 
-visual_tags_enabled = True
-if "--disable-visual-tag" in sys.argv:
-    visual_tags_enabled = False
-
-continuous_estimation = False
-if "--continuous-estimation" in sys.argv:
-    continuous_estimation = True
-
-_est = est.Estimation (continuous_estimation=continuous_estimation,
-        joint_states_topic=topic,
-        visual_tags_enabled=visual_tags_enabled)
-_est.estimation_rate = estimation_rate
+_est = est.Estimation(
+    continuous_estimation=args.continuous_estimation,
+    joint_states_topic=args.joint_state,
+    visual_tags_enabled=not args.disable_visual_tags,
+)
+_est.estimation_rate = args.estimation_rate
 _est.spin()
